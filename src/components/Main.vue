c70cbfb3e81e3999f4996a909eb6f8c6f9735b66
Preil <preil2000@gmail.com>
2019-07-29 14:14:35 +0300

    Navigation bar added.

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container>
+        <v-layout
+                text-center
+                wrap
+        >
+            <v-flex xs12>
+
+            </v-flex>
+
+            <v-flex mb-4>
+                <h1 class="display-2 font-weight-bold mb-3">
+                    Welcome
+                </h1>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,78 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <nav>
+        <v-app-bar text app>
+            <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span>Ninja</span>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-menu>
+                <template v-slot:activator="{ on }">
+                    <v-btn text v-on="on" color="grey">
+                        <v-icon left></v-icon>
+                        <span>Menu</span>
+                    </v-btn>
+                </template>
+
+                <v-list>
+                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-item-title>{{ link.text }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+
+            <v-btn text color="grey">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-app-bar>
+        <v-navigation-drawer app v-model="drawer" class="primary">
+            <v-layout column align-center>
+                <v-flex class="mt-5">
+                    <v-avatar size="100">
+                        <img src="/avatar-1.png"/>
+                    </v-avatar>
+                </v-flex>
+                <p class="subheading white--text mt-1 align-center">Ninja</p>
+
+
+            </v-layout>
+            <v-list>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-item-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+
+    export default {
+        name: "Navbar",
+
+        data() {
+            return {
+                drawer: false,
+                links: [
+                    {icon: 'dashboard', text: 'Dashboard', route: '/'},
+                    {icon: 'folder', text: 'My projects', route: '/projects'},
+                    {icon: 'person', text: 'Team', route: '/team'},
+                ],
+                snackbar: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
